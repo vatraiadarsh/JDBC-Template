1ae3db5eaf42d195ffd5cf92a3bf9fa732c2f5c7
Adarsha <vatraiadarsh@gmail.com>
2021-07-20 12:23:01 +1000

    Testing mysql driver

---
+++ manage.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Testing mysql driver"
+git push -u origin jdbcMysql
---
+++ src/main/java/com/ada/jdbcmysql/Program.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ada.jdbcmysql;
+
+/**
+ *
+ * @author vatrai
+ */
+public class Program {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
